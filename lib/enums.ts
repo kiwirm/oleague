20765c5ecad67efe92d7311a90166f452e1d39b1
Ryan Moore <kiwi.ryanmoore@gmail.com>
2025-01-28 12:01:55 +1300

    add DQ status and change some icons

--- lib/enums.ts
+++ lib/enums.ts
@@ -1,15 +1,15 @@
 export const disciplines = {
-  FOR: { icon: "forest", tooltip: "Forest Event" },
-  SPR: { icon: "location_city", tooltip: "Sprint Event" },
-  MTB: { icon: "directions_bike", tooltip: "Mountain Bike Event" },
-  NGT: { icon: "partly_cloudy_night", tooltip: "Night Event" },
-  SCO: { icon: "workspaces", tooltip: "Score Event" },
+  FOR: { icon: "forest", tooltip: "Forest event" },
+  SPR: { icon: "location_city", tooltip: "Sprint event" },
+  MTB: { icon: "directions_bike", tooltip: "Mountain bike event" },
+  NGT: { icon: "partly_cloudy_night", tooltip: "Night event" },
+  SCO: { icon: "workspaces", tooltip: "Score event" },
 };
 
 export const statusGrades = {
   RUNNING: { icon: "sprint", tooltip: "In Grade" },
-  RUNNING_UP: { icon: "arrow_upward", tooltip: "Running Up A Grade" },
-  RUNNING_DOWN: { icon: "arrow_downward", tooltip: "Running Down A Grade" },
+  RUNNING_UP: { icon: "arrow_upward", tooltip: "Running up a grade" },
+  RUNNING_DOWN: { icon: "arrow_downward", tooltip: "Running down a grade" },
   ORGANISING: { icon: "construction", tooltip: "Organiser" },
   RUNNING_OTHER: {
     icon: "question_mark",
@@ -23,10 +23,11 @@ export const statusResults = {
   CTRL: { icon: "inventory", tooltip: "Controller", color: "bg-yellow-100" },
   OK: { icon: "check", tooltip: "OK", color: "bg-inherit" },
   MP: { icon: "mp", tooltip: "Mispunch", color: "bg-inherit" },
-  NA: { icon: "close", tooltip: "DNF or Mispunch", color: "bg-inherit" },
-  DNF: { icon: "close", tooltip: "Did Not Finish", color: "bg-inherit" },
-  DNS: { icon: "close", tooltip: "Did Not Start", color: "bg-inherit" },
-  NW: { icon: "close", tooltip: "No Eligible Winner", color: "bg-inherit" },
+  NA: { icon: "rule", tooltip: "Some races missed", color: "bg-inherit" },
+  DNF: { icon: "close", tooltip: "Did not finish", color: "bg-inherit" },
+  DNS: { icon: "close", tooltip: "Did not start", color: "bg-inherit" },
+  NW: { icon: "error", tooltip: "No eligible winner", color: "bg-inherit" },
+  DQ: { icon: "block", tooltip: "Disqualified", color: "bg-inherit" },
 };
 
 export const eligibility = {
