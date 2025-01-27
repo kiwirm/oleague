722a7d2ac44be40c126b5e7fa61bfa5d9b0ffba6
Ryan Moore <kiwi.ryanmoore@gmail.com>
2025-01-28 12:10:00 +1300

    expand search to all orienteers that have competed in at least one league event

--- app/orienteers/page.tsx
+++ app/orienteers/page.tsx
@@ -1,6 +1,5 @@
 import OrienteerSearch from "./search";
 
-import { Alert, AlertDescription, AlertTitle } from "@/components/alert";
 import Navbar from "@/components/navbar";
 import Title from "@/components/title";
 
@@ -9,11 +8,9 @@ import prisma from "@/lib/prisma";
 const OrienteerSearchPage = async ({ showNavbar = true }) => {
   const orienteers = await prisma.orienteer.findMany({
     where: {
-      member: {
-        every: {
-          club_id: {
-            equals: "PP",
-          },
+      NOT: {
+        result: {
+          none: {},
         },
       },
     },
@@ -24,23 +21,15 @@ const OrienteerSearchPage = async ({ showNavbar = true }) => {
     <>
       {showNavbar && (
         <Navbar
-          breadcrumbLinks={[{ text: "Orienteers", href: "/orienteers" }]}
+          breadcrumbLinks={[
+            { text: "oleagues.nz", href: "/" },
+            { text: "Orienteers", href: "/orienteers" },
+          ]}
         />
       )}
       <Title>Orienteers</Title>
       <div>
         <OrienteerSearch orienteers={orienteers} />
-        <Alert variant="warning" className="mt-4">
-          <AlertTitle>
-            <span className="material-symbols-rounded align-text-bottom pr-1">
-              Warning
-            </span>
-            Not fully implemented
-          </AlertTitle>
-          <AlertDescription>
-            Search implemented for PAPO orienteers only
-          </AlertDescription>
-        </Alert>
       </div>
     </>
   );
